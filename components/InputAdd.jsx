87c436b5734c942cfad898ad4a944063910a8d7c
David Sliren <bluee.97.berry@gmail.com>
2025-12-03 18:07:47 -0500

    feact: Implementar las siguientes librerias de expo: linear-gradient, blur, font

--- components/InputAdd.jsx
+++ components/InputAdd.jsx
@@ -1,16 +1,27 @@
 import { View, Text, TextInput, Pressable } from "react-native";
 import { MotiView } from "moti";
 import React from "react";
+import { Easing } from "react-native-reanimated";
 import tw from "twrnc";
 const InputAdd = () => {
   return (
     <MotiView
-      style={tw`bg-sky-700 flex-row items-center`}
-      from={{ opacity: 0, scale: 0 }}
-      animate={{ opacity: 1, scale: 1 }}
+      from={{ translateY: -20 }}
+      animate={{ translateY: 20 }}
+      transition={{
+        type: "timing",
+        duration: 800,
+        easing: Easing.bounce,
+      }}
+      style={tw`flex-row justify-between gap-2 bg-sky-700`}
     >
-      <TextInput placeholder="Agrega una tarea" />
-      <Pressable style={tw`bg-red-500 h-full`}>
+      <TextInput placeholder="Agrega una tarea" style={tw`w-full`} />
+      <Pressable
+        onPress={() => console.log("hola")}
+        style={({ pressed }) => [
+          tw`${pressed ? "bg-red-500" : "bg-blue-500"} `,
+        ]}
+      >
         <Text>Add</Text>
       </Pressable>
     </MotiView>
