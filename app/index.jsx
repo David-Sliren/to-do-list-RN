e55f0151f7597767b988431ab7a03d3b5a66b5b9
David Sliren <bluee.97.berry@gmail.com>
2025-12-03 23:54:41 -0500

    style: Modificar parte del diseño principal

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,14 @@
 import { View, Text } from "react-native";
-import React from "react";
 import tw from "twrnc";
 import { MotiView } from "moti";
 import InputAdd from "../components/InputAdd";
 const index = () => {
   return (
-    <View style={tw`flex-1 justify-center items-center bg-sky-200`}>
-      <Text style={tw`text-3xl font-bold`}>Lista de tareas</Text>
+    <View style={tw`flex-1 justify-evenly items-center bg-purple-500/60`}>
+      <View
+        style={tw`absolute top-16 overflow-hidden w-full h-full bg-sky-200 rounded-t-20`}
+      ></View>
+      <Text style={tw`text-4xl font-extrabold`}>Lista de tareas</Text>
       <InputAdd />
     </View>
   );
