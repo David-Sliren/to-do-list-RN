048c67348ffdb44264a28ed40eefb93de9c8e939
David Sliren <bluee.97.berry@gmail.com>
2025-12-04 00:14:30 -0500

    style: Mejorar parte del layout

--- app/index.jsx
+++ app/index.jsx
@@ -4,12 +4,13 @@ import { MotiView } from "moti";
 import InputAdd from "../components/InputAdd";
 const index = () => {
   return (
-    <View style={tw`flex-1 justify-evenly items-center bg-purple-500/60`}>
+    <View style={tw`flex-1 bg-purple-500/60`}>
       <View
-        style={tw`absolute top-16 overflow-hidden w-full h-full bg-sky-200 rounded-t-20`}
-      ></View>
-      <Text style={tw`text-4xl font-extrabold`}>Lista de tareas</Text>
-      <InputAdd />
+        style={tw`absolute top-16 justify-around items-center overflow-hidden w-full h-full bg-sky-200 rounded-t-20`}
+      >
+        <Text style={tw`text-4xl font-extrabold`}>Lista de tareas</Text>
+        <InputAdd />
+      </View>
     </View>
   );
 };
