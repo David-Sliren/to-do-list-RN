a25366221214cbe425a3262cdbd2e001e5891792
David Sliren <bluee.97.berry@gmail.com>
2025-12-04 20:03:38 -0500

    chore-style: rediceñar estructura y modificar estilos de los componentes y la screen index.jsx

--- app/index.jsx
+++ app/index.jsx
@@ -1,23 +1,38 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import tw from "twrnc";
 import { MotiView } from "moti";
 import InputAdd from "../components/InputAdd";
 import Section from "../components/Section";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { LinearGradient } from "expo-linear-gradient";
 const index = () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <View style={tw`flex-1 bg-purple-400`}>
+      <LinearGradient
+        colors={["transparent", "#aa6eaa"]}
+        locations={[0.8, 1]}
+        start={{ x: 0.5, y: 1 }}
+        end={{ x: 0.5, y: 0 }}
+        style={tw`flex-1`}
+      >
         <View
-          style={tw`absolute top-4 justify-center items-center gap-10 overflow-hidden w-full h-full bg-white rounded-t-xl`}
+          style={tw` flex-1 absolute top-8 py-4 items-center overflow-hidden w-full h-full bg-white/45 rounded-t-10 border-2 border-white`}
         >
-          <Text style={tw`text-sky-500 text-5xl font-extrabold`}>
-            Lista de tareas
-          </Text>
-          <InputAdd />
-          <Section></Section>
+          <View style={tw`mb-10 mt-10 items-center gap-8`}>
+            <Text style={tw`text-sky-500 text-5xl font-extrabold`}>
+              Lista de tareas
+            </Text>
+            <InputAdd />
+          </View>
+          <ScrollView
+            contentContainerStyle={tw`items-center p-2`}
+            style={tw`w-[95%] flex-1 min-h-120`}
+          >
+            <Section />
+            <Section />
+          </ScrollView>
         </View>
-      </View>
+      </LinearGradient>
     </SafeAreaView>
   );
 };
--- components/Section.jsx
+++ components/Section.jsx
@@ -6,11 +6,11 @@ const Section = ({ children, title = "Tareas Rapidas", NumTasks = 0 }) => {
   return (
     <LinearGradient
       colors={["transparent", "blueviolet"]}
-      locations={[0.91, 1]}
-      style={tw`overflow-hidden w-11/12 h-120 rounded-xl`}
+      locations={[0.7, 1]}
+      style={tw`flex-1 overflow-hidden w-full h-120 border-2 mb-15`}
     >
       <View
-        style={tw`absolute bottom-4 gap-2 overflow-hidden bg-zinc-400/45 w-full h-full px-4 pt-8 rounded-b-3xl`}
+        style={tw`absolute bottom-12 gap-2 overflow-hidden bg-zinc-200/45 w-full h-full px-4 pt-16 rounded-b-12 border-2 border-white`}
       >
         <View style={tw`flex-row gap-1 items-center`}>
           <Text style={tw`text-black text-lg font-bold`}>{title}</Text>
