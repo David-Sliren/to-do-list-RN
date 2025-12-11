47a899a8df9ac8a4b78c9722edbb953354d529e8
David Sliren <bluee.97.berry@gmail.com>
2025-12-11 00:09:39 -0500

    feact(Stotage):    Implementar guardado local de las tareas

--- context/TasksContext.jsx
+++ context/TasksContext.jsx
@@ -1,8 +1,12 @@
 // React
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 import { View, Text } from "react-native";
+import asyncStorage from "@react-native-async-storage/async-storage";
 
 const Tasks = createContext();
+
+// Llaves para el almacenamiento
+const TAsKS_FLASH = "@TasksFlash";
 
 const TasksContext = ({ children }) => {
   // Tareas
@@ -12,6 +16,44 @@ const TasksContext = ({ children }) => {
 
   // Caracteristicas Desativadas
   const [isAvilable, setIsAvilable] = useState(false);
+
+  // Almacenamiento local
+  const [isLoading, setIsLoading] = useState(true);
+
+  // Leer tarea de el almacenamiento
+  const getData = async () => {
+    try {
+      const value = await asyncStorage.getItem(TAsKS_FLASH);
+      if (value !== null) {
+        const data = JSON.parse(value);
+        setTasks(data);
+      }
+    } catch (e) {
+      console.log("Fallo en el guardado" + e);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  // Guardar tarea en el almacenamiento
+  const saveData = async (taskSave) => {
+    try {
+      const value = JSON.stringify(taskSave);
+      await asyncStorage.setItem(TAsKS_FLASH, value);
+    } catch (e) {
+      console.log("Fallo en el guardado" + e);
+    }
+  };
+
+  useEffect(() => {
+    if (!isLoading) {
+      saveData(tasks);
+    }
+  }, [tasks, isLoading]);
 
   // Agregar tarea
   function addTask() {
@@ -34,6 +76,7 @@ const TasksContext = ({ children }) => {
     setIsAvilable(true);
   }
 
+  // Valor del contexto
   const valor = {
     tasks,
     addTask,
