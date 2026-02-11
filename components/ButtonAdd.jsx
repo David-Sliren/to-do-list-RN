d0e0b5b178917163f6b9b22fe6082cefa39c336c
David Sliren <bluee.97.berry@gmail.com>
2026-02-10 19:35:48 -0500

    style(components/ButtonAdd): Cambiar fondo y agregar componente blur

--- components/ButtonAdd.jsx
+++ components/ButtonAdd.jsx
@@ -6,16 +6,22 @@ import { Ionicons } from "@expo/vector-icons";
 
 // Librerias
 import tw from "twrnc";
-import { colorBody } from "../constants/colorsPrincipals";
+import { BlurView } from "expo-blur";
 
 const ButtonAdd = ({ action }) => {
   return (
-    <Pressable
-      onPress={() => action()}
-      style={tw`absolute bottom-20 right-4 bg-black/85 justify-center items-center size-15 rounded-full z-1`}
+    <BlurView
+      intensity={40}
+      tint="light"
+      style={tw`absolute bottom-20 right-4 border border-white/50 size-15 rounded-full overflow-hidden z-1`}
     >
-      <Ionicons name="add" style={[tw`text-2xl`, { color: colorBody.aqua }]} />
-    </Pressable>
+      <Pressable
+        onPress={() => action()}
+        style={tw` justify-center items-center size-full `}
+      >
+        <Ionicons name="add" style={tw`text-2xl text-black`} />
+      </Pressable>
+    </BlurView>
   );
 };
 
