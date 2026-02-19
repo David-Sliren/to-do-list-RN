61b64e41572fb2aa091299ea58e64e3ed1fff6d5
David Sliren <bluee.97.berry@gmail.com>
2026-02-19 18:32:47 -0500

    feact: Agregar persistencia de datos

--- store/shopping/shopping.js
+++ store/shopping/shopping.js
@@ -1,8 +1,23 @@
+// Zustand
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
+
+// Slices
 import { useShoppingProducts } from "../slices/shopping.products";
 import { useShoppingStores } from "../slices/shopping.stores";
 
-export const useShoppingCombine = create((...a) => ({
-  ...useShoppingProducts(...a),
-  ...useShoppingStores(...a),
-}));
+// Almacenamiento
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export const useShoppingCombine = create(
+  persist(
+    (...a) => ({
+      ...useShoppingProducts(...a),
+      ...useShoppingStores(...a),
+    }),
+    {
+      name: "shopping-store",
+      storage: createJSONStorage(() => AsyncStorage),
+    },
+  ),
+);
